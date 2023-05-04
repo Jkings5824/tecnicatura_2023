3ea2c7876414314a73671d00b2795670e62349f3
Sergio Javier Reyes <102986627+Jkings5824@users.noreply.github.com>
2023-05-04 18:29:30 -0300

    Clase 2 python completa

--- Python/Archivos/Leccion2/manejo_archivos.py
+++ Python/Archivos/Leccion2/manejo_archivos.py
@@ -4,6 +4,9 @@ try:
     archivo.write('Programamos con diferentes tipos de archivos, ahora en txt.\n')
     archivo.write('Los acentos son importantes para las palabras\n')
     archivo.write('como por ejemplo: acción, ejecución y producción,\n')
+    archivo.write('Las letras son:\n "r" read,\n "a" append,\n "w" write,\n "x" crea un archivo,')
+    archivo.write('\n "t" para texto o text, \n "b" tipo binario(imagenes, video etc.)\n "w+" y "r+" lee y escribe son iguales,\n')
+    archivo.write('saludos a los alumnos de la tecnicatura\n')
     archivo.write('Con esto terminamos')
     
 except Exception as e:
